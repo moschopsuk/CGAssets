9ab56d961bb2363450cc6868e1ec4ccea495ef7e
Luke Moscrop <moschopsuk@gmail.com>
2015-06-06 19:34:33 +0100

    Added basic hello world to test express & nodejs

---
+++ app/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app 	= express();
+
+//Serve content out of public dir
+app.use(express.static('public'));
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
+
