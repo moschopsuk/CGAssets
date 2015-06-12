803b102dee5349a2464619675f660b4a23ab3624
Luke Moscrop <moschopsuk@gmail.com>
2015-06-12 13:21:59 +0100

    Now using UI-View for view composting with ngRoute

--- app/public/js/dashboard.js
+++ app/public/js/dashboard.js
@@ -2,6 +2,20 @@ var app = angular.module('DashboardApp', ['ngRoute']);
 
 app.controller('DashCtrl', ['$scope', '$log', '$location',
     function($scope, $log, $location){
-    	$log.info("LA1TV CG Dashboard Started");
+        $log.info("LA1TV CG Dashboard Started");
+    }
+]);
+
+
+app.config(['$routeProvider',
+    function($routeProvider) {
+
+        $routeProvider
+            .when("/lowerthirds", {
+                templateUrl: '/modules/lowerthirds/admin.html',
+                controller: 'lowerthirdsCtrl'
+            })
+        .otherwise({redirectTo: '/lowerthirds'});
+
     }
 ]);
---
+++ app/public/modules/lowerthirds/controller.js
@@ -0,0 +1,5 @@
+app.controller('lowerthirdsCtrl', ['$scope',
+    function($scope){
+        
+    }
+]);
