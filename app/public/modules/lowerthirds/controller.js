cc91625b178d3387a7955063be39af9df67db7d6
Luke Moscrop <moschopsuk@gmail.com>
2015-06-14 10:42:28 +0100

    Added lower thirds dashboard options

--- app/public/modules/lowerthirds/controller.js
+++ app/public/modules/lowerthirds/controller.js
@@ -1,5 +1,23 @@
 app.controller('lowerthirdsCtrl', ['$scope',
     function($scope){
-        
+
+ 		$scope.queuedThirds = [];
+
+        $scope.add = function(item) {
+            $scope.queuedThirds.push(item);
+
+            $scope.lowerThirdsForm.$setPristine();
+            $scope.lowerThird = {};
+        };
+
+        $scope.remove = function(index){
+            $scope.queuedThirds.splice(index, 1);
+        };
+
+        $scope.show = function(side, item) {
+        };
+
+        $scope.hide = function() {
+        };
     }
 ]);
