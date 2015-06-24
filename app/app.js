ade046d0c5e83ca8c1a6413a93382f7d765e3b25
Luke Moscrop <moschopsuk@gmail.com>
2015-06-24 12:52:47 +0100

    Formatting

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var socket	= require('socket.io');
-var app 	= express();
-var env 	= require('node-env-file');
+var socket  = require('socket.io');
+var app     = express();
+var env     = require('node-env-file');
 
 //Load up the .env file
 env(__dirname + '/.env');
@@ -10,42 +10,45 @@ env(__dirname + '/.env');
 app.use(express.static('public'));
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('LA1:TV CG app listening at http://%s:%s', host, port);
+    console.log('LA1:TV CG app listening at http://%s:%s', host, port);
 });
 
 var io = socket.listen(server);
 
-var bug 		= {}
-, 	grid		= {}
-,	lowerthird 	= {}
+var bug         = {}
+,   grid        = {}
+,   lowerthird  = {}
 ;
 
 io.on('connection', function(socket) {
-	console.log("Client Socket Connected");
+    console.log("Client Socket Connected");
 
-	//send init states for the CG
-	socket.emit("lowerthird", lowerthird);
-	socket.emit("grid", grid);
-	socket.emit("bug", bug);
+    //send init states for the CG
+    socket.emit("lowerthird", lowerthird);
+    socket.emit("grid", grid);
+    socket.emit("bug", bug);
 
-	//Forward data to frontend
-	socket.on("lowerthird", function(payload) {
-		lowerthird = payload;
-		io.sockets.emit("lowerthird", payload);
-	});
+    //Forward data to frontend
+    socket.on("lowerthird", function(payload) {
+        lowerthird = payload;
+        io.sockets.emit("lowerthird", payload);
+        console.log("Updating: lowerthird");
+    });
 
-	socket.on("grid", function(payload) {
-		grid = payload;
-		io.sockets.emit("grid", payload);
-	});
+    socket.on("grid", function(payload) {
+        grid = payload;
+        io.sockets.emit("grid", payload);
+        console.log("Updating: grid");
+    });
 
-	socket.on("bug", function(payload) {
-		bug = payload;
-		io.sockets.emit("bug", payload);
-	});
+    socket.on("bug", function(payload) {
+        bug = payload;
+        io.sockets.emit("bug", payload);
+        console.log("Updating: bug");
+    });
 });
 
 
